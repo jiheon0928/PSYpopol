7409aa3267534d41f5d9e85156687b8faf161345
jiheon <wwlgjs77@gmail.com>
2025-02-19 10:49:58 +0900

    ..

--- js/parallax.js
+++ js/parallax.js
@@ -4,8 +4,8 @@ const timeline4 = gsap.timeline({
   scrollTrigger: {
     trigger: ".tigger_Parallax",
     start: "top top",
-    end: "bottom top",
-    markers: true,
+    end: "+=10000 top",
+
     scrub: true,
     pin: true,
     duration: 4,
@@ -15,45 +15,45 @@ const timeline4 = gsap.timeline({
 timeline4.fromTo(
   ".panation_1",
   { scale: 0, opacity: 1 },
-  { scale: 50, opacity: 0, duration: 4 }
+  { scale: 10, opacity: 0, duration: 4 }
 );
 timeline4.fromTo(
   ".panation_2",
   { scale: 0, opacity: 1 },
-  { scale: 50, opacity: 0, duration: 4 }
+  { scale: 10, opacity: 0, duration: 4 }
 );
 timeline4.fromTo(
   ".panation_3",
   { scale: 0, opacity: 1 },
-  { scale: 100, opacity: 0, duration: 4 }
+  { scale: 10, opacity: 0, duration: 4 }
 );
 timeline4.fromTo(
   ".panation_4",
   { scale: 0, opacity: 1 },
-  { scale: 100, opacity: 0, duration: 4 }
+  { scale: 10, opacity: 0, duration: 4 }
 );
 timeline4.fromTo(
   ".panation_5",
   { scale: 0, opacity: 1 },
-  { scale: 100, opacity: 0, duration: 4 }
+  { scale: 10, opacity: 0, duration: 4 }
 );
 timeline4.fromTo(
   ".panation_6",
   { scale: 0, opacity: 1 },
-  { scale: 100, opacity: 0, duration: 4 }
+  { scale: 10, opacity: 0, duration: 4 }
 );
 timeline4.fromTo(
   ".panation_7",
   { scale: 0, opacity: 1 },
-  { scale: 100, opacity: 0, duration: 4 }
+  { scale: 10, opacity: 0, duration: 4 }
 );
 timeline4.fromTo(
   ".panation_8",
   { scale: 0, opacity: 1 },
-  { scale: 100, opacity: 0, duration: 4 }
+  { scale: 10, opacity: 0, duration: 4 }
 );
 timeline4.fromTo(
   ".panation_9",
   { scale: 0, opacity: 1 },
-  { scale: 100, opacity: 0, duration: 4 }
+  { scale: 10, opacity: 0, duration: 4 }
 );
